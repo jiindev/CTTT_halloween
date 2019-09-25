4353bfbd2124ab1df1f0cf7cc55120886f1127f1
jiindev <jiindev@gmail.com>
2019-09-25 17:55:34 +0900

    페이지 전환 구현 / 랜덤 운세결과 출력
    마우스 클릭에 따른 페이지 전환 및 div 페이드 인/아웃을 구현함. 케이크를 클릭 시 랜덤으로 운세결과가 출력되도록 정리.

--- fortune/js/fortune.js
+++ fortune/js/fortune.js
@@ -1,10 +1,15 @@
 //start 버튼 클릭 시 사라지도록
 let start_btn = document.querySelector('.start_btn');
 let start_page = document.querySelector('.start_page');
+let cake_page = document.querySelector('.cake_page');
 start_btn.onclick = function () {
     start_page.classList.add('animated', 'fadeOut');
     setTimeout(function () {
         start_page.remove();
+    }, 1000);
+    setTimeout(function () {
+        cake_page.style.display='block';
+        cake_page.classList.add('animated', 'bounceInDown');
     }, 1000);
 };
 
@@ -18,14 +23,31 @@ document.onmousemove=function(){
 
 }
 
-//랜덤으로 포츈결과 가져오기
-// let cake = document.querySelector('.cake');
-// let quote = document.querySelector('.quote');
-// cake.onclick = function () {
-//     newQuote();
-// }
-// let newQuote = function () {
-//     let index = Math.floor(Math.random() * quotes.length);
-//     quote.innerHTML = quotes[index];
-// }
-// let quotes = ["좋은 결과", "보통 결과", "나쁜 결과"];
+//클릭 시 갈라지는 케이크
+let cake = document.querySelector('.cake');
+let left_cake=document.querySelector('.left_cake');
+let cake_open=false;
+let cake_dregs=document.querySelector('.cake_dregs');
+
+cake.onclick=function(){
+    if(!cake.classList.contains('moved')){
+        cake.classList.add('moved');
+        newQuote();
+        cake_dregs.style.display='block';
+        cake_dregs.classList.add('animated','fadeIn','slow');
+        setTimeout(function () {
+            fork.classList.add('animated','fadeOut','faster');
+        }, 500);
+        setTimeout(function () {
+            fork.style.display='none';
+        }, 1000);
+    }
+}
+
+//랜덤 결과
+let quote = document.querySelector('.quote');
+let newQuote = function () {
+    let index = Math.floor(Math.random() * quotes.length);
+    quote.innerHTML = quotes[index];
+}
+let quotes = ["좋은 결과", "보통 결과", "나쁜 결과"];
