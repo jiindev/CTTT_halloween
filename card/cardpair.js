5f5285a3a365c8117df090d0e4192e29fcde6ce1
jiindev <jiindev@gmail.com>
2019-10-19 18:46:28 +0900

    카드게임 1차 소스변경

--- card/cardpair.js
+++ card/cardpair.js
@@ -2,7 +2,7 @@
 
 let hor = 4;
 let ver = 3;
-let colors = ['red','red','orange','orange','green','green','yellow','yellow','white','white','pink','pink']
+let colors = ['url("./img/cardStage1a.png")','url("./img/cardStage1a.png")','url("./img/cardStage1b.png")','url("./img/cardStage1b.png")','url("./img/cardStage1c.png")','url("./img/cardStage1c.png")','url("./img/cardStage1d.png")','url("./img/cardStage1d.png")','url("./img/cardStage1e.png")','url("./img/cardStage1e.png")','url("./img/cardStage1f.png")','url("./img/cardStage1f.png")']
 let colorList = colors.slice();
 let color = [];
 let clickFlag = true; 
@@ -30,7 +30,7 @@ function cardSetting (hor, ver){
         cardFront.className="card-front";
         let cardBack = document.createElement('div');
         cardBack.className="card-back";
-        cardBack.style.backgroundColor=color[i];
+        cardBack.style.backgroundImage=color[i];
         cardInner.appendChild(cardFront);
         cardInner.appendChild(cardBack);
         card.appendChild(cardInner);
@@ -39,15 +39,24 @@ function cardSetting (hor, ver){
                 if(clickFlag && !completeCard.includes(c)){
                     card.classList.toggle('flipped');
                     clickCard.push(c);
+                    if(clickCard[0]===clickCard[1]){
+                        clickCard=[];
+                    }
                     if(clickCard.length===2){
-                        if(clickCard[0].querySelector('.card-back').style.backgroundColor===clickCard[1].querySelector('.card-back').style.backgroundColor){
+                        if(clickCard[0].querySelector('.card-back').style.backgroundImage===clickCard[1].querySelector('.card-back').style.backgroundImage){
                             completeCard.push(clickCard[0]);
                             completeCard.push(clickCard[1]);
+                            let firstCard = clickCard[0];
+                            let secondCard = clickCard[1];
+                            setTimeout(function(){
+                                firstCard.style.opacity='.5';
+                                secondCard.style.opacity='.5';
+                            },800);
                             clickCard=[];
                             if(completeCard.length === 12){
                                 let finishTime = new Date();
                                 alert('성공!'+(finishTime-startTime)/1000+'초 걸림!');
-                                document.querySelector('#wrapper').innerHTML='';
+                                document.querySelector('.cards').innerHTML='';
                                 colorList = colors.slice();
                                 color = [];
                                 completeCard = [];
@@ -69,20 +78,29 @@ function cardSetting (hor, ver){
             })
         })(card);
         
-        document.querySelector('#wrapper').appendChild(card);
+        document.querySelector('.cards').appendChild(card);
     }
     document.querySelectorAll('.card').forEach(function(card, index){
+        card.style.transform='rotate('+Math.floor(Math.random() * 15)+'deg)';
         setTimeout(function(){
-            card.classList.add('flipped');
+            card.style.opacity=1;
         }, 1000+100*index);
     })
     setTimeout(function(){
         document.querySelectorAll('.card').forEach(function(card, index){
-            card.classList.remove('flipped');
+            setTimeout(function(){
+                card.classList.add('flipped');
+            }, 1000+100*index);
         })
-        clickFlag = true;
-        startTime = new Date();
-    }, 5000);
+        setTimeout(function(){
+            document.querySelectorAll('.card').forEach(function(card, index){
+                card.classList.remove('flipped');
+            })
+            clickFlag = true;
+            startTime = new Date();
+        }, 5000);
+    },3000);
+    
 }
 shuffle();
 cardSetting(hor,ver);
